3b6a85d6f207040db0f5b9b8a0cfbac62fca12a4
Villy-P <valeriuspetrini@gmail.com>
2024-11-11 13:45:16 -0600

    refactor: add namespace to Plot class and improve formatting

--- Core/Plot.cs
+++ Core/Plot.cs
@@ -1,22 +1,24 @@
-public class Plot {
-    public bool isVisible = false;
-    public bool isFlagged = false;
-    public int row, col;
-    public string content = "0";
+namespace ASCII_Minesweeper.Core {
+    public class Plot {
+        public bool isVisible = false;
+        public bool isFlagged = false;
+        public int row, col;
+        public string content = "0";
 
-    public Plot(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
+        public Plot(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
 
-    public string GetValue() {
-        if (this.isFlagged)
-            return "⚐";
-        else if (!this.isVisible)
-            return " ";
-        else if (this.content == "0")
-            return ".";
-        else 
-            return this.content;
+        public string GetValue() {
+            if (this.isFlagged)
+                return "⚐";
+            else if (!this.isVisible)
+                return " ";
+            else if (this.content == "0")
+                return ".";
+            else 
+                return this.content;
+        }
     }
 }
