2b3d645a3c0db63fa1e669a117068f3673921f39
Villy-P <valeriuspetrini@gmail.com>
2025-06-24 16:15:03 -0500

    refactor: simplify Plot class constructor and improve XML documentation

--- Core/Plot.cs
+++ Core/Plot.cs
@@ -1,19 +1,15 @@
 namespace ASCII_Minesweeper.Core {
-    public class Plot {
+    /// <summary>
+    /// Initializes a new instance of the Plot class with specified row and column.
+    /// </summary>
+    /// <param name="row">The row index of the plot.</param>
+    /// <param name="col">The column index of the plot.</param>
+    public class Plot(int row, int col)
+    {
         public bool isVisible = false;
         public bool isFlagged = false;
-        public int row, col;
+        public int row = row, col = col;
         public string content = "0";
-
-        /// <summary>
-        /// Initializes a new instance of the Plot class with specified row and column.
-        /// </summary>
-        /// <param name="row">The row index of the plot.</param>
-        /// <param name="col">The column index of the plot.</param>
-        public Plot(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
 
         /// <summary>
         /// Sets the content of the plot to a mine.
